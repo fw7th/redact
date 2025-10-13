6592fdc7536b5ec9e4f68d9531709efb953dc4a5
fw7th <okosa7th@gmail.com>
2025-10-13 15:00:00 +0100

    stage1 complete

--- api/main.py
+++ api/main.py
@@ -1,27 +1,56 @@
-from typing import Union
+import os
+import shutil
+from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, File, HTTPException, Response, UploadFile
+from fastapi.staticfiles import StaticFiles
 
-app = FastAPI()
+BASE_DIR = "uploads"
 
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+def create_base():
+    try:
+        # Ensure the base directory exists when the application starts
+        os.makedirs(BASE_DIR, exist_ok=True)
+        return {"message": "Base directory created successfully."}
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Failed to create directory: {str(e)}"
+        )
+
+
+# Define the lifespan context manager
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup events: Code here runs when the application starts
+    print("Application startup: Initializing resources...")
+    create_base()
+    # Example: database connection, loading models, etc.
+    yield
+    # Shutdown events: Code here runs when the application shuts down
+    print("Application shutdown: Cleaning up resources...")
+    # Example: closing database connections, releasing resources
+
+
+app = FastAPI(lifespan=lifespan)
+app.mount("/images", StaticFiles(directory="uploads"), name="images")
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def favicon():
+    return Response(status_code=204)  # No Content
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/uploadfile/")
+async def upload_image(file: UploadFile = File(...)):
+    try:
+        with open(f"uploads/{file.filename}", "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
 
+    except Exception:
+        raise HTTPException(status_code=500, detail="Could not upload file")
+    finally:
+        await file.close()  # Ensure the file is closed
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.price, "item_id": item_id}
+    return {"filename": file.filename, "content_type": file.content_type}
